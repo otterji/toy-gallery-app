2c36ecdf71e44af51a84a15fe520109b088c3b71
otterji <youngsdg1@gmail.com>
2021-11-30 19:16:38 +0900

    feat: myGallery grid

--- src/screens/MyGalleryScreen/index.js
+++ src/screens/MyGalleryScreen/index.js
@@ -1,10 +1,35 @@
-import { Box, Button, Center, Input, Text } from 'native-base';
+import { Box, SimpleGrid, Center, Text } from 'native-base';
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import DefaultBtn from '../../components/DefaultBtn';
 import authActions from '../../store/auth/actions';
 import colors from '../../styles/colors';
+const screen = Dimensions.get('window')
+const items = 12;
+const data = Array(items).fill(0);
+
+const myGallery = [{
+  id: 0,
+  title: '0',
+  lastUpdated: '',
+},
+{
+  id: 1,
+  title: '1',
+  lastUpdated: '',
+},
+{
+  id: 2,
+  title: '2',
+  lastUpdated: '',
+},
+{
+  id: 3,
+  title: '3',
+  lastUpdated: '',
+},
+];
 
 
 function MyGalleryScreen({ navigation, route }) {
@@ -14,11 +39,41 @@ function MyGalleryScreen({ navigation, route }) {
   // const { user, registerLoading } = useSelector(state => state.authReducer);
 
   return (
-    <Box paddingX="15px" width="100%" height="100%">
-      <Box>
-        <Text fontSize="14px" marginTop="25px" >MyGallery</Text>
-      </Box >
-    </Box>
+
+    <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start' }}>
+      <View style={{ width: '50%', paddingLeft: 10, paddingRight: 5, paddingTop: 10, paddingBottom: 10 }}>
+        <Box style={{ borderWidth: 3, height: 200 }}>
+          <Text>dkssudsdafdsfas</Text>
+        </Box>
+      </View>
+      <View style={{ width: '50%', paddingLeft: 5, paddingRight: 5, paddingTop: 10, paddingBottom: 10 }}>
+        <Box style={{ borderWidth: 3, height: 200 }}>
+          <Text>dkssudsdafdsfas</Text>
+        </Box>
+      </View>
+      <View style={{ width: '50%', paddingLeft: 10, paddingRight: 5, paddingTop: 10, paddingBottom: 10 }}>
+        <Box style={{ borderWidth: 3, height: 200 }}>
+          <Text>dkssudsdafdsfas</Text>
+        </Box>
+      </View>
+    </View>
+    // <Box paddingX="15px" width="100%" height="100%">
+    //   <Box style={{ width: "100%" }}>
+    //     <Center flex={1}>
+    //       {/* {myGallery.map((x, idx) => {
+    //         return <>
+    //           <SimpleGrid columns={2} spacing={3} style={{ width: "100%" }}>
+    //             <Box key={idx} style={{ borderWidth: 2, width: 150, height: 150 }} rounded="sm">
+    //               <Text>{x.title}</Text>
+    //             </Box>
+    //           </SimpleGrid>
+    //         </>
+    //       })} */}
+
+
+    //     </Center>
+    //   </Box >
+    // </Box>
   )
 }
 
