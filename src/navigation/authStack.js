9cdbbfa5bd403cb565cad4b9bd586b8b98cca615
otterji_desktop <otterji@pulit.co.kr>
2022-01-16 03:01:01 +0900

    refactor: remove unused components

--- src/navigation/authStack.js
+++ src/navigation/authStack.js
@@ -4,8 +4,6 @@ import LoginScreen from '../screens/LoginScreen';
 import colors from '../styles/colors';
 import SignupScreen from '../screens/SignupScreen';
 import NameScreen from '../screens/NameScreen';
-import SignupCompletedScreen from '../screens/SignUpCompletedScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { navigate } from './route';
 import { Toast } from 'native-base';
 import { getItemFromAsync } from '../hooks/requests';
@@ -43,7 +41,6 @@ export function AuthStack() {
       <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Sign in' }} />
       <Stack.Screen name="SignUp" component={SignupScreen} options={{ title: 'Sign up' }} />
       <Stack.Screen name="Name" component={NameScreen} options={{ title: 'Sign up' }} />
-      <Stack.Screen name="SignUpCompleted" component={SignupCompletedScreen} options={{ title: 'Welcome' }} />
     </Stack.Navigator>
   );
 }
